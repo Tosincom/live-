deee73768585dd4aef4c52a513811378e282a67a
tosin <tafolabi956@gmail.com>
2023-04-07 10:37:46 -0700

    we are live

---
+++ index.js
@@ -0,0 +1,13 @@
+let v1 = [1, 2, 3];
+let v2 = [2, 3, 4];
+
+function dot_product() {
+    let dot_product = v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]
+    console.log(dot_product)
+}
+
+dot_product()
+
+if ("v1*v2 = 0") {
+console.log ("vectors are orthogonal")
+}
