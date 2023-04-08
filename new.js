b3aff6fb382efaed9f288da865d04aa2e982473c
tosin <tafolabi956@gmail.com>
2023-04-07 22:38:07 -0700

    block 1

---
+++ new.js
@@ -0,0 +1,17 @@
+let arr = [1, 5, 7, 9, 4, 3 ]
+
+function insertion(){
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+          }
+          arr[j + 1] = current;
+      }
+      return arr
+      console.log(insertion())
+}
+
+insertion()
